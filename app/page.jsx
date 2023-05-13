1ec7b9659dfaeb969f146a5be6fa3985cdc241ab
ilham <ibouaik@gmail.com>
2023-05-13 21:38:23 +0100

    create components files

---
+++ app/page.jsx
@@ -0,0 +1,22 @@
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Discover & share
+            <br className="max-md:hidden" />
+            <span className="orange_gradient text-center">
+                AI-Powered Prompts
+            </span>
+        </h1>
+        <p className="desc text-center">
+            Promptopiz is an open-source AI 
+            prompting tool for modren world to discover,
+            create and share creative prompts.
+        </p>
+
+        {/* Feed */}
+    </section>
+  )
+}
+
+export default Home
