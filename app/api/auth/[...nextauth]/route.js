7b0180ed9f9684635f92421e5c90069e8a1e9a19
ilham <ibouaik@gmail.com>
2023-05-15 12:30:37 +0100

    googgle provider

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,28 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { signIn } from "next-auth/react"
+
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        })
+    ],
+
+    async session({session}) {
+
+    },
+
+    async signIn({profile}) {
+        try {
+
+        } catch {
+
+        }
+    }
+
+})
+
+export {handler as GET, handler as POST}
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import Navbar from '@/components/Navbar'
+import Provider from '@/components/Provider'
 import '@/styles/globals.css'
 
 export const metadata = {
@@ -8,16 +9,17 @@ export const metadata = {
 const RootLayout = ({children}) => {
   return (
     <html lang="en">
-        <body>
-            <div className="main">
-                <div className="gradient" />
-            </div>
-
-            <main className="app">
-                <Navbar />
-                {children}
-            </main>
-        </body>
+        <Provider>
+            <body>
+                <div className="main">
+                    <div className="gradient" />
+                </div>
+                <main className="app">
+                    <Navbar />
+                    {children}
+                </main>
+            </body>
+        </Provider>
     </html>
   )
 }
