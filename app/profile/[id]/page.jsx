5ed38b4b5c902d295d7239015a55500399f36283
ilham <ibouaik@gmail.com>
2023-05-17 12:21:23 +0100

    user profile

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -1,8 +1,36 @@
+'use client'
 
-const UserProfile = () => {
+import Profile from "@/components/Profile"
+import { useEffect, useState } from "react"
+import { useSearchParams } from "next/navigation";
+
+
+const UserProfile = ({params}) => {
+  console.log(params)
+ const [userposts, setUserPosts] = useState([])
+ const searchParams = useSearchParams()
+ const userName = searchParams.get("name")
+
+ useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${params?.id}/posts`)
+      const data = await response.json();
+
+
+      setUserPosts(data)
+    }
+    if (params?.id) fetchPosts();
+  }, [params?.id])
+  
   return (
-    <div>UserProfile </div>
+     <Profile
+      name={userName}
+      desc={`Welcome to ${userName}'s personalized profile page. Explore ${userName}'s exceptional prompts and be inspired by the power of their imagination`}
+      data={userposts}
+    />
+   
   )
 }
 
 export default UserProfile
+
